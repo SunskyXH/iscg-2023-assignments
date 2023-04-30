1dd202f47a440ba150b2383dcc43d7f0a8185fa2
SunskyXH <sunskyxh@gmail.com>
2023-04-30 21:03:02 +0900

    chore: tweak style

--- app/m1/page.tsx
+++ app/m1/page.tsx
@@ -13,7 +13,7 @@ export default function M1() {
   return (
     <main className="flex h-screen items-center p-24 pr-0">
       <div className="flex h-full w-60 flex-shrink-0 flex-col gap-y-4">
-        <div>
+        <div className="px-5 py-4">
           <h1 className="text-xl font-bold">基本課題M1</h1>
           <span className="text-sm opacity-50">パラメトリック曲線</span>
         </div>
