e69e5e6b361640d897a3eb33aed9d8bc8326b4fd
SunskyXH <sunskyxh@gmail.com>
2023-04-30 14:08:02 +0900

    chore: tweak style

--- app/m1/page.tsx
+++ app/m1/page.tsx
@@ -8,7 +8,11 @@ export const metadata = {
 export default function M1() {
   return (
     <main className="flex h-screen items-center p-24 pr-0">
-      <div className="h-full w-60 flex-shrink-0">
+      <div className="flex h-full w-60 flex-shrink-0 flex-col gap-y-4">
+        <div>
+          <h1 className="text-xl font-bold">基本課題M1</h1>
+          <span className="text-sm opacity-50">パラメトリック曲線</span>
+        </div>
         <SidebarItem title="Bézier curve" desc="n-order Bézier curve" active />
       </div>
       <Canvas />
